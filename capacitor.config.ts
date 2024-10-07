c5e605e17b7dc71aaa60686057f19d428bdecb7c
prathameshvibhute <prathuvibhute3110@gmail.com>
2024-10-07 19:16:24 +0530

    project initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cric_score',
+  webDir: 'www'
+};
+
+export default config;

64ed8bf8b971928651724299cf62c2dc668be533
prathameshvibhute <prathuvibhute3110@gmail.com>
2024-10-07 19:25:22 +0530

    Created layout directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './layout/auth/auth.component';
+import { HomeComponent } from './layout/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "auth",
+    component: AuthComponent
   },
+  {
+    path: "home",
+    component: HomeComponent
+  }
 ];
 
 @NgModule({

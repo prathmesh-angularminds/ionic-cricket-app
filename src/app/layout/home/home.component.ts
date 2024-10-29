0019f574fda6c027ad09e5ffc72c0abfc07796bb
prathameshvibhute <prathuvibhute3110@gmail.com>
2024-10-29 14:06:11 +0530

    Added show hide functionality for navbar

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,22 @@ export class HomeComponent  implements OnInit {
 
   playersButton: boolean = false;
   matchesButton: boolean = true;
-  constructor() { }
+  showNavbar: boolean = false;
+  constructor(private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events.pipe(filter((event: any) => event instanceof NavigationEnd)).subscribe((route: any) => {
+      if(route.url.includes('player-list') || route.url.includes('match-list')) {
+        this.showNavbar = true;
+        this.setActiveNavbarButton(route.url.split('/')[2]);
+      } else {
+        this.showNavbar = false
+      }
+    })
+  }
 
   setActiveNavbarButton(label: string) {
-    if(label === 'players') {
+    if(label === 'player-list') {
       this.playersButton = true;
       this.matchesButton = false;
     } else {

dff83bbe1d93436e417f88d3e77fdc6c474d29a2
prathameshvibhute <prathuvibhute3110@gmail.com>
2025-01-11 12:25:16 +0530

    Updated match summary page

--- src/app/view/home/match-summary/match-summary.component.ts
+++ src/app/view/home/match-summary/match-summary.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Data } from 'src/app/models/data';
 import { FirebaseService } from 'src/app/service/firebase.service';
 
@@ -20,24 +20,41 @@ export class MatchSummaryComponent  implements OnInit {
     },
     {
       label: "Delete",
-      url: "/home/match-list",
+      url: "/home/match-summary",
       queryParam: {}
     }
   ]
+  matchId: string = "";
+  matchCardId: string = "";
+  showDeleteMatchModal: boolean = false;
+  
+
   constructor(
     public data: Data,
     public firebase: FirebaseService,
-    public activatedRoute: ActivatedRoute
+    public activatedRoute: ActivatedRoute,
+    public router: Router
   ) { }
 
   ngOnInit() {
-    this.data.matchId.subscribe((id: string) => {
-      this.getMatchData(id);
+    this.activatedRoute.queryParams.subscribe({
+      next: (param: any) => {
+        this.dropDownList[1].queryParam = {showDeleteMatchModal: true, id: param.id,matchCardId:  param.matchCardId}
+        this.matchId = param.id;
+        this.matchCardId = param.matchCardId;
+        this.showDeleteMatchModal = param?.showDeleteMatchModal || false;
+        this.getMatchData(param.id);
+      }
     })
   }
 
   transformElement(num: number) {
     this.showCard = num
+  }
+
+  deleteMatch() {
+    this.firebase.deleteMatch(this.matchId,this.matchCardId);
+    this.router.navigate(['/home/match-list']);
   }
 
   returnPlayerHalfFirstName(firstName: string, lastName: string) {

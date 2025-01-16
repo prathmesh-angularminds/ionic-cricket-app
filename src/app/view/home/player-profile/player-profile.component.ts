ec364e899fb0eb5a979bbcaede001e6b7a30646a
prathameshvibhute <prathuvibhute3110@gmail.com>
2025-01-16 17:23:42 +0530

    Added dropdown for player-profile-list

--- src/app/view/home/player-profile/player-profile.component.ts
+++ src/app/view/home/player-profile/player-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Player } from 'src/app/models/player.interface';
 import { FirebaseService } from 'src/app/service/firebase.service';
 
@@ -13,13 +13,47 @@ export class PlayerProfileComponent  implements OnInit {
   pageTitle: string = "Player Statistics";
   url: string = "/home/player-list";
   player!: any;
+  dropDownList: any = [
+    {
+      label: "Edit player",
+      url: "/home/edit-player",
+      queryParam: {}
+    },
+    {
+      label: "Reset player",
+      url: "/home/player-profile",
+      queryParam: {}
+    },
+    {
+      label: "Delete player",
+      url: "/home/player-profile",
+      queryParam: {}
+    },
+  ];
+  playerId: string= "";
+  showDeletePlayerModal: boolean = false;
+  showResetPlayerModal: boolean = false;
+  shouldCallGetApi: boolean = true;
   
-  constructor(private activatedRoute: ActivatedRoute, private firebaseService: FirebaseService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private firebaseService: FirebaseService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe({
       next: (param: any) => {
-        this.getPlayerData(param.id);
+        this.playerId = param.id;
+        this.dropDownList[0].queryParam = {id: param.id};
+        this.dropDownList[1].queryParam = {id: param.id,showResetPlayerModal: true, shouldCallGetApi: false};
+        this.dropDownList[2].queryParam = {id: param.id,showDeletePlayerModal: true, shouldCallGetApi: false};
+        this.showDeletePlayerModal = param?.showDeletePlayerModal || false;
+        this.showResetPlayerModal = param?.showResetPlayerModal || false;
+        if(this.shouldCallGetApi) {
+          this.getPlayerData(param.id);
+          this.shouldCallGetApi = false;
+        }
       },
       error: () => {},
       complete: () => {}
@@ -28,11 +62,57 @@ export class PlayerProfileComponent  implements OnInit {
 
   getPlayerData(id: string) {
     this.firebaseService.getPlayerById(id).subscribe((player: Player) => {
-      this.player = player;
-      this.calculateBattingAverage();
-      this.calculateBowlingAverage();
-      this.calculateFieldingAverage();
+      if(player) {
+        this.player = player;
+        this.calculateBattingAverage();
+        this.calculateBowlingAverage();
+        this.calculateFieldingAverage();
+      }
     });
+  }
+
+  deletePlayer() {
+    this.firebaseService.deletePlayer(this.playerId).subscribe({
+      next: () => {
+        this.router.navigate(['/home/player-list'])
+      }
+    })
+  }
+
+  resetPlayer() {
+    let player = {
+      id: this.playerId,
+      battingDetails: {
+          four: 0,
+          average: 0,
+          ball: 0,
+          strikeRate: 0,
+          dismissedCount: 0,
+          six: 0,
+          run: 0
+      },
+      fieldingDetails: {
+          catch: 0,
+          runOutAttempt: 0,
+          catchTaken: 0,
+          fieldingAvg: 0,
+          catchDroped: 0
+      },
+      match: 0,
+      bowlingDetails: {
+          run: 0,
+          over: 0,
+          mostWicket: 0,
+          average: 0,
+          wicket: 0,
+          economy: 0
+      }
+  }
+    this.firebaseService.updatePlayer(player).subscribe({
+      next: () => {
+        this.getPlayerData(this.playerId);
+      }
+    })
   }
 
   calculateBattingAverage() {
